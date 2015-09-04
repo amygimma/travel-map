b2c1a601b9ee86ffd2902172586a729efd9d7bbd
Amy Gimma <amyg225@gmail.com>
2015-09-04 16:19:20 -0400

    center map

--- app/assets/javascripts/components/maps.js
+++ app/assets/javascripts/components/maps.js
@@ -3,21 +3,12 @@ if($('#map').length > 0) {
     var mapCanvas = document.getElementById('map');
     var mapOptions = {
       center: new google.maps.LatLng(44.5403, -78.5463),
-      zoom: 8,
+      zoom: 6,
       mapTypeId: google.maps.MapTypeId.HYBRID
     };
 
     var map = new google.maps.Map(mapCanvas, mapOptions);
-    console.log(map);
-    // debugger;
-    var marker;
-    function createMarker(coords, map, title){
-      marker = new google.maps.Marker({
-        position: coords,
-        map: map,
-        title: title
-      });
-    }
+
 
     var trip_id = $("#trip_id").data("tripId");
     $.ajax({
@@ -27,11 +18,12 @@ if($('#map').length > 0) {
         var coords = { lat: place.latitude, lng: place.longitude };
         var title = place.name;
         var blog_url = place.blog_url;
-        new google.maps.Marker({
+        var marker = new google.maps.Marker({
           position: coords,
           map: map,
           title: title
         });
+        map.panTo(marker.getPosition());
       });
     });
   }
